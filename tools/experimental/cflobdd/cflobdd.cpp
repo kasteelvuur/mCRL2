c6213e6315c383acad344e30bc22fedbef396e02
kasteelvuur <kasteelvuur@gmail.com>
2025-04-05 17:11:59 +0200

    Add level and out degree to proto-CFLOBDD

--- tools/experimental/cflobdd/cflobdd.cpp
+++ tools/experimental/cflobdd/cflobdd.cpp
@@ -8,45 +8,84 @@
 //
 /// \file cflobdd.cpp
 
+#include <numeric>
 
 #include "mcrl2/atermpp/aterm.h"
 
 
-using namespace atermpp; 
+using namespace atermpp;
+using namespace std;
 
 
-class cflobdd: public aterm
+class proto_cflobdd: public aterm
 {
+  private:
+    const uint32_t level;
+    const uint32_t out_degree;
+
   public:
-    cflobdd(const function_symbol& f)
-      : aterm(f)
-    {}
+    /**
+     * Constant proto-CFLOBDD I or V
+     * @param out_degree The out degree of this proto-CFLOBDD
+     */
+    proto_cflobdd(const uint32_t& out_degree)
+      : aterm(function_symbol((out_degree == 1) ? "I" : "V", 0)), level(1), out_degree(out_degree)
+    {
+      // Out degree must be 1 or 2 for I or V respectively
+      assert(1 <= out_degree <= 2);
+    }
 
-    cflobdd(const function_symbol& f, const cflobdd& c1, const cflobdd& c2)
-      : aterm(f,c1,c2)
-    {}
+    /**
+     * Proto-CFLOBDD inductive case (L, [L_0, ..., L_{n-1}], m)
+     * @param c The proto-CFLOBDD L
+     * @param cvs The list of proto-CFLOBDDs [L_0, ..., L_{n-1}] and mapping m merged into a vector of pairs.
+     * Each proto-CFLOBDD L_i is paired with a vector of mapping result values v_i such that
+     * L_i.out_degree = v_i.size() and v_i[j] = m(i,j).
+     */
+    proto_cflobdd(const proto_cflobdd& c, const vector<pair<proto_cflobdd, vector<uint32_t>>>& cvs):
+      aterm(function_symbol("C", 2), c, cvs),
+      level(c.level + 1),
+      out_degree(std::accumulate(
+        cvs.begin(),
+        cvs.end(),
+        0,
+        [](const uint32_t& max, const pair<proto_cflobdd, vector<uint32_t>>& cv) {
+          return std::max(max, *std::max_element(get<1>(cv).begin(), get<1>(cv).end()));
+        }
+      ))
+    {
+      // The out degree of the first proto-CFLOBDD must match the number of remaining proto-CFLOBDDs
+      assert(c.out_degree == cvs.size());
+
+      for (pair<proto_cflobdd, vector<uint32_t>> cv : cvs) {
+        // All proto-CFLOBDs must have the same level
+        assert(c.level == get<0>(cv).level);
+
+        // The out degree of each proto-CFLOBDD L_i must match the amount of corresponding return values
+        assert(get<0>(cv).out_degree == get<1>(cv).size());
+      }
+
+      // TODO: Actually store c and cvs in some way. Possibly in separate class?
+    }
 };
 
-class cflobdd_i: public cflobdd
+class proto_cflobdd_i: public proto_cflobdd
 {
-  // Constructor
-  cflobdd_i()
-    : cflobdd(function_symbol("i", 0))
+  proto_cflobdd_i()
+    : proto_cflobdd(1)
   {}
 };
 
-class cflobdd_v: public cflobdd
+class proto_cflobdd_v: public proto_cflobdd
 {
-  // Constructor
-  cflobdd_v()
-    : cflobdd(function_symbol("v", 0))
+  proto_cflobdd_v()
+    : proto_cflobdd(2)
   {}
 };
 
-class cflobdd_c: public cflobdd
+class proto_cflobdd_c: public proto_cflobdd
 {
-  // Constructor
-  cflobdd_c(const cflobdd& c1, const cflobdd& c2)
-    : cflobdd(function_symbol("c", 2), c1, c2)
+  proto_cflobdd_c(const proto_cflobdd& c, const vector<pair<proto_cflobdd, vector<uint32_t>>>& cvs)
+    : proto_cflobdd(c, cvs)
   {}
 };
